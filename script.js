38f78a98ecbf7e43e741d84496d054db7d846b96
pokeyuke <augustogc134@gmail.com>
2024-11-06 19:07:54 +0100

    Created the divs for the button and the slider in the html file.
    Created the whole js file. Defined the the function that creates the grid sells and colours them.
    
    Added some more style and the overall layout of the divs in the styles.css file

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = "repeat(16, 1fr)";
-board.style.gridTemplateRows = "repeat(16, 1fr)";
+const GRIDSIDE = 600;
+let rows = 16;
+let cols = 16;
+const sliderContainer = document.querySelector("#slide-container") ;
+const slider = document.querySelector("#slider")
+const sliderValue = document.querySelector("#sliderValue")
 
-for (let i = 0; i<=256; i++){
-    const pixel = document.createElement("div")
-    pixel.style.backgroundColor = "blue"
-    board.insertAdjacentElement("beforeEnd", pixel)
+let squaresPerSide = 16;
+sliderValue.textContent = `${squaresPerSide} x ${squaresPerSide} (resolution)`;
+
+
+
+
+    const board = document.querySelector("#board")
+    board.style.width = board.style.height - `${GRIDSIDE}`
+    
+
+
+
+
+function setBackgroundColor() {
+    this.style.backgroundColor = "purple";
 }
 
- 
+function createGridCells(squaresPerSide) {
+    const numOfSquares = (squaresPerSide * squaresPerSide);
+    const widthOrHeight = `${(GRIDSIDE / squaresPerSide) - 2}px`;
+
+    for (let i = 0; i < numOfSquares; i++ ) {
+        const gridCell = document.createElement("div");
+
+        gridCell.style.width = gridCell.style.height = widthOrHeight;
+        gridCell.classList.add("cell");
+
+        board.appendChild(gridCell);
+
+        gridCell.addEventListener("mouseover", setBackgroundColor)
+    }
+}
+
+createGridCells(squaresPerSide)
+
+function removeGridCells() {
+    while (board.firstChild) {
+        board.removeChild(board, firstChild);
+    }
+
+}
+
+createGridCells(16)
